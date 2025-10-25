d6dc5ec021c2ac81b41b518e20ba806aed0857d3
Lucas Moraes <lucas.moraes88@yahoo.com>
2025-10-25 11:29:55 -0300

    login pacial

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -4,27 +4,46 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Field, FieldGroup, FieldLabel } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
-import { useForm } from "@tanstack/react-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const inputform = [
+  {
+    name: "username",
+    label: "Nome de usuário",
+    type: "text",
+    autoComplete: "off",
+  },
+  {
+    name: "password",
+    label: "Senha",
+    type: "password",
+    autoComplete: "off",
+  },
+];
+
+const formSchema = z.object({
+  username: z.string({ message: "Nome de usuário é obrigatório" }).trim(),
+  password: z.string({ message: "Senha é obrigatória" }).min(8, "A senha deve ter pelo menos 8 caracteres"),
+});
 
 export const LoginPage = () => {
-  const form = useForm({
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
     defaultValues: {
       username: "",
       password: "",
     },
-    onSubmit: async ({ value }) => {
-      console.log(`=> form onSubmit`, value);
-    },
   });
+
+  const onSubmit = (data: z.infer<typeof formSchema>) => {
+    console.log("Form Data:", data);
+  };
 
   return (
     <div className="fixed w-full h-full flex justify-center items-center bg-[url('/src/assets/login-background.webp')] bg-cover bg-center">
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          form.handleSubmit();
-        }}
-      >
+      <form onSubmit={form.handleSubmit(onSubmit)}>
         <Card className="w-[300px] bg-blue-500/10 backdrop-blur-sm border border-blue-500/20">
           <CardHeader>
             <CardTitle>Login</CardTitle>
